59a55ce15ae8fbdf7db91c3643bc7df07b90406c
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-11-22 15:41:00 +0300

    setup views

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 
 const express = require('express')
+const MainRouter = require('./routers/Main')
+const path = require('path')
 
 const app = express()
 
-app.get('/', (request, response) => response.send('hello world'))
-
+app.set('views', path.join(__dirname, '/views'));
+app.use('/', MainRouter)
 app.listen(3000, () => {
     console.log("Server is waiting for a connection...")
 })
