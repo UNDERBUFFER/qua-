cd9a9c6d97e1c9f877178711cfdc6edfb2fa29a9
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-12-05 20:18:13 +0300

    add login and registration panels

--- src/controllers/Auth.js
+++ src/controllers/Auth.js
@@ -1,8 +1,7 @@
 
 class Auth {
     static get(request, response) {
-        response.send('hello world')
-        // response.render('main/main.hbs');
+        response.render('auth/main.hbs')
     }
     static post(request, response) {
         response.send('tmp fuck you')
