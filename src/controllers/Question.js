45afd7899dbef387195c0f8a95418cdebeb560c6
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-12-27 22:59:05 +0300

    adding question

--- src/controllers/Question.js
+++ src/controllers/Question.js
@@ -6,8 +6,15 @@ class Question {
         response.render('question/main.hbs')
     }
     static post(request, response) {
-        console.log(request.body)
-        response.send('ok')
+        request.body.author = request.user
+        const question = new QuestionModel(request.body)
+        question.save((error) => {
+            if (error) {
+                console.log(error)
+                response.redirect('/')
+            }
+            else response.redirect(`/question/${question.id}/`)
+        })
     }
 }
 
