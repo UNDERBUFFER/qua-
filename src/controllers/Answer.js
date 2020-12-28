1a3ee64f6e0c0d1d9826e274683086fd41a19f64
UNDERBUFFER <ruslanekitsky@gmail.com>
2020-12-28 22:11:42 +0300

    add first version of answer url

--- src/controllers/Answer.js
+++ src/controllers/Answer.js
@@ -1,10 +1,25 @@
 
 const AnswerModel = require('../models/Answer')
+const Question = require('../models/Question')
 
 class Answer {
     static post(request, response) {
-        console.log(request.body)
-        response.send('ok')
+        const questionId = request.params.id
+        let question = null
+        Question.find({ '_id': questionId }, (error, result) => {
+            if (error) console.log(error)
+            if (result.length > 0) question = result[0]
+            else response.redirect('/')
+            const answer = new AnswerModel({
+                text: request.body.text,
+                author: request.user,
+                question
+            })
+            answer.save((error) => {
+                if (error) console.log(error)
+                response.redirect(`/question/${questionId}`)
+            })
+        })
     }
 }
 
